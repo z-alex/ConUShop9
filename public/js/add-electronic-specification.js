dd3b5fc02aac4f04bc76e7e66e72796c0349a228
KarineZhang <karine.zhang@hotmail.com>
2017-11-20 03:40:47 -0500

    Fixed 2 bugs in regex

--- public/js/add-electronic-specification.js
+++ public/js/add-electronic-specification.js
@@ -155,7 +155,7 @@ function loadForm() {
         </div>
 
         <div class="form-group">
-            <label class="control-label col-sm-2" for="weight">Weight(kg)/label>
+            <label class="control-label col-sm-2" for="weight">Weight(kg)</label>
             <div class="col-sm-10">
                 <input type="text"  class="form-control" id="weight" placeholder="Enter weight" name="weight">
             </div>

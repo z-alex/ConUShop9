3a5b03154959e72ebe87b45da60ac5c73bef10ae
Karine Zhang <karine.zhang@hotmail.com>
2017-11-23 00:16:02 -0500

    Testing for PaymentTDGTest

---
+++ tests/Unit/PaymentTDGTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Classes\TDG\PaymentTDG;
+use App\Classes\Core\Payment;
+
+class PaymentTDGTest extends TestCase {
+
+	function testInsert(){
+		$paymentTDG = new PaymentTDG();
+		$payment = new Payment();
+		$paymentData = new \stdClass();
+		$paymentData->amount = '1000';
+
+		$payment->set($paymentData);
+		$paymentTDG->insert($payment);
+
+		 $this->assertDatabaseHas('payment', [
+            'amount' => '1000',
+        ]);
+
+	}
+}

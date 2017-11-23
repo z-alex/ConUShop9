ac74231567740205313ebc95820ad32523dc19b2
Karine Zhang <karine.zhang@hotmail.com>
2017-11-23 01:15:14 -0500

    Implemented test for ReturnTransaction class

---
+++ tests/Unit/ReturnTransactionClassTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Classes\Core\ReturnTransaction;
+
+
+class ReturnTransactionClassTest extends TestCase {
+	
+	function testSetGet(){
+		$returnTransaction = new ReturnTransaction();
+		$returnData = new \stdClass();
+		$returnData->id = 1;
+		$returnData->User_id = 7;
+		$returnData->ElectronicItem_id = 7;
+		$returnData->isComplete = 1;
+
+		$returnTransaction->set($returnData); 
+		$retrievedRTData = $returnTransaction->get();
+
+		$this->assertTrue($retrievedRTData->id == $returnData->id,
+		$retrievedRTData->User_id == $returnData->User_id,
+		$retrievedRTData->ElectronicItem_id == $returnData->ElectronicItem_id,
+		$retrievedRTData->isComplete == $returnData->isComplete
+		);
+	}
+}
